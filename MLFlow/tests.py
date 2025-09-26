c97b217838ead068c842e2ff4aef8469911ff50d
Khatir Youyou <khatir.youyou@epita.fr>
2025-09-26 16:33:19 +0200

    feat: canary deployment done

--- MLFlow/tests.py
+++ MLFlow/tests.py
@@ -4,18 +4,40 @@ import requests
 # api-endpoint
 TEST1 = "http://0.0.0.0:8000/predict"
 TEST2 = "http://0.0.0.0:8000/update-model"
+TEST3 = "http://0.0.0.0:8000/accept-next-model"
 
 # defining a params dict for the parameters to be sent to the API
 data_predict = {"X_pred": [5.7,3.8,1.7,0.3]}
-data_update = {"version" : 2}
+data_update1 = {"version" : 1}
+data_update2 = {"version" : 2}
+
 
 # sending get request and saving the response as response object
-request2 = requests.post(url= TEST2, json = data_update)
-request3 = requests.post(url = TEST1, json = data_predict)
+request1 = requests.post(url= TEST1, json = data_predict)
+request2 = requests.post(url= TEST2, json = data_update1)
+request3 = requests.post(url= TEST1, json = data_predict) # Tjr le meme modèle
+request4 = requests.post(url=TEST3, json='') # On change le modèle
+request5 = requests.post(url= TEST1, json = data_predict) # On predict sur le nouveau modèle
+request6 = requests.post(url= TEST2, json = data_update2)
+request7 = requests.post(url=TEST3, json='') # On change le modèle
+
+
+
+
 
 # extracting data in json format
+data1 = request1.text
 data2 = request2.text
 data3 = request3.text
+data4 = request4.text
+data5 = request5.text
+data6 = request6.text
+data7 = request7.text
 
+print(data1)
 print(data2)
 print(data3)
+print(data4)
+print(data5)
+print(data6)
+print(data7)
--- MLFlow/web_service.py
+++ MLFlow/web_service.py
@@ -5,9 +5,13 @@ import os
 import numpy as np
 import script_training
 import mlflow
+import random
 from mlflow.tracking import MlflowClient
 
 mlflow.set_tracking_uri(uri=os.getenv("MLFLOW_TRACKING_URI", "http://127.0.0.1:8080"))
+
+current_model = mlflow.sklearn.load_model(f"models:/tracking-quickstart/1")
+next_model = mlflow.sklearn.load_model(f"models:/tracking-quickstart/1")
 
 class NumbersRequest(BaseModel):
     X_pred: List[float]
@@ -17,15 +21,23 @@ class VersionRequest(BaseModel):
 
 app = FastAPI()
 
+@app.post("/predict")
+def predict(payload: NumbersRequest, p: float = 0.7):
+    model = current_model if random.random() < p else next_model
+    arr = np.asarray([payload.X_pred], dtype=np.float64)
+    result = model.predict(arr)
+    return {"Prediction": result.tolist()}
+
+
 @app.post("/update-model")
 def update_model(data: VersionRequest):
     model_uri = f"models:/tracking-quickstart/{data.version}"
-    script_training.lr = mlflow.sklearn.load_model(model_uri)
-    return {"message": "Model updated successfully", "version": data.version}
+    global next_model
+    next_model = mlflow.sklearn.load_model(model_uri)
+    return {"message": "Next model updated successfully", "version": data.version}
 
-
-@app.post("/predict")
-def predict(payload: NumbersRequest):
-    arr = np.asarray([payload.X_pred], dtype=np.float64)  # (1,4)
-    result = script_training.lr.predict(arr)
-    return {"Prediction": result.tolist()}
+@app.post('/accept-next-model')
+def accept_next_model():
+    global current_model, next_model
+    current_model = next_model
+    return {"status": "next model accepted as current"}
